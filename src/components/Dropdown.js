30b325a87d4bdc2072c43a9d3fa2902ed1c189b8
anil kadam <anilkadam210@gmail.com>
2020-09-05 00:16:04 +0530

    More JSX - to show dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,25 @@
 import React from "react";
 
-const Dropdown = () => {
-  return <h1>Dropdown</h1>;
+const Dropdown = ({ options }) => {
+  const renderedOptions = options.map((option) => {
+    return (
+      <div className='item' key={option.value}>
+        {option.label}
+      </div>
+    );
+  });
+  return (
+    <div className='ui form'>
+      <div className='field'>
+        <label className='label'>select a color</label>
+        <div className='ui selection dropdown visible active'>
+          <i className='dropdown icon'></i>
+          <div className='text'>Select Color</div>
+          <div className='menu visible transition'>{renderedOptions}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dropdown;
