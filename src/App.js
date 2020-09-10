7652898dcb5c280a33de4f6020da32d9aed48d06
anil kadam <anilkadam210@gmail.com>
2020-09-10 21:07:15 +0530

    onSelected option handling - dropdown

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
@@ -38,11 +38,16 @@ const options = [
   },
 ];
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
       {/* <Accordion items={items} /> */}
       {/* <Search /> */}
-      <Dropdown options={options} />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const Dropdown = ({ options }) => {
+const Dropdown = ({ options, selected, onSelectedChange }) => {
   const renderedOptions = options.map((option) => {
     return (
-      <div className='item' key={option.value}>
+      <div
+        className='item'
+        key={option.value}
+        onClick={() => onSelectedChange(option)}>
         {option.label}
       </div>
     );
@@ -14,7 +17,7 @@ const Dropdown = ({ options }) => {
         <label className='label'>select a color</label>
         <div className='ui selection dropdown visible active'>
           <i className='dropdown icon'></i>
-          <div className='text'>Select Color</div>
+          <div className='text'>{selected.label}</div>
           <div className='menu visible transition'>{renderedOptions}</div>
         </div>
       </div>
